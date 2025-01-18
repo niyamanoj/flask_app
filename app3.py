52a0762c587111b9423c4116632fea47b2fa43be
Niya Manoj <you@example.com>
2025-01-18 23:10:48 +0530

    caretocure

--- app3.py
+++ app3.py
@@ -23,7 +23,7 @@ app = Flask(__name__)
 def home():
     return "Welcome to the Chronic Disease Prediction API!"
 
-@app.route('/predict', methods=['GET','POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Parse the input JSON
